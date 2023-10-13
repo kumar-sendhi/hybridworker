ec057f7a256f36c996ba0854e2e03f0024ae26b3
kumar-sendhi <65189074+kumar-sendhi@users.noreply.github.com>
2023-10-13 12:24:31 -0500

    Create hybridworkser.ps1

---
+++ hybridworkser.ps1
@@ -0,0 +1,67 @@
+param(
+
+[Parameter (Mandatory= $true)]
+
+[string]
+
+ $hybridworkergroup,
+
+[Parameter (Mandatory= $true)]
+
+[string]
+
+$AutomationAccountName,
+
+[Parameter (Mandatory= $true)]
+
+[string]
+
+$VmResourceId,
+
+[Parameter (Mandatory= $true)]
+
+[string]
+
+$ResourceGroupName,
+
+[Parameter (Mandatory= $true)]
+
+[string]
+
+$registerationURL,
+
+[Parameter (Mandatory= $true)]
+
+[string]
+
+$Location,
+
+[Parameter (Mandatory= $true)]
+
+[string]
+
+$vm_name,
+
+[Parameter (Mandatory= $true)]
+
+[string]
+
+$extensionname
+
+)
+
+ 
+
+ 
+
+$abc=[guid]::NewGuid()
+
+New-AzAutomationHybridRunbookWorker -AutomationAccountName $AutomationAccountName -Name $abc -HybridRunbookWorkerGroupName $hybridworkergroup -VmResourceId $VmResourceId -ResourceGroupName $ResourceGroupName
+
+ $settings = @{
+
+"AutomationAccountURL"  = $registerationURL;
+
+};
+
+Set-AzVMExtension -ResourceGroupName $ResourceGroupName -Location $Location -VMName $vm_name -Name $extensionname -Publisher "Microsoft.Azure.Automation.HybridWorker" -ExtensionType HybridWorkerForWindows -Settings $settings -EnableAutomaticUpgrade $false -TypeHandlerVersion 1.10
